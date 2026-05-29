96f282917094f8a2b30fb6681cb45434cddba5cc
Niklas Karoli <niklas@tiqqet.net>
2026-05-29 18:51:01 +0200

    Delete src directory

--- src/utils/scoring.ts
+++
@@ -1,5 +0,0 @@
-export const calculateScore = (remainingTime: number, totalTime: number): number => {
-  if (remainingTime < 0) return 0;
-  // Punkte = round(500 + (500 * Restzeit / Gesamtzeit))
-  return Math.round(500 + (500 * (remainingTime / totalTime)));
-};
