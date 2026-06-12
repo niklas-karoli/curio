ee528d727c28ae348cdd320a2db58230feedef87
Niklas Karoli <niklas@tiqqet.net>
2026-06-12 17:38:28 +0200

    Update HostView.tsx

--- src/pages/HostView.tsx
+++ src/pages/HostView.tsx
@@ -86,7 +86,7 @@ export const HostView = () => {
       clean: true,
       keepalive: 30,
       reconnectPeriod: 2000,
-      clientId: `curio_${isHost ? 'host' : 'client'}_${peerId}`
+      clientId: `curio_host_${Math.random().toString(36).substring(2, 9)}`
     });
 
     clientRef.current = client;
